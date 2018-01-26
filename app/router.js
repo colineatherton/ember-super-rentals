72d591adddc3f0f14f88fe7f15592df1de9441d6
Colin Eatherton <colineatherton@Colins-MacBook-Pro.local>
2018-01-26 09:27:05 +0000

    completed tutorial

--- app/router.js
+++ app/router.js
@@ -1,4 +1,4 @@
-import EmberRouter from '@ember/routing/router';
+  import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
 const Router = EmberRouter.extend({
@@ -7,6 +7,11 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('about');
+  this.route('contact');
+  this.route('rentals', function() {
+    this.route('show', { path: '/:rental_id' });
+  });
 });
 
 export default Router;
